c72f48df228f956d783e5bc70ea99add114dbf64
nizheg <nzhegalin@gmail.com>
2015-02-15 20:37:40 +0400

    Icons are added and some other small refactorings

--- src/me/nizheg/en/MainActivity.java
+++ src/me/nizheg/en/MainActivity.java
@@ -2,6 +2,7 @@ package me.nizheg.en;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -17,6 +18,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     public void onToggleClicked(View view) {
