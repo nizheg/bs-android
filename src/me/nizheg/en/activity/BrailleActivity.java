d3fcaa3f36f0b0aea79fe2c9acd621f2513a8af7
nizheg <nzhegalin@gmail.com>
2015-02-16 18:39:28 +0300

    Move braille from main activity to separate

---
+++ src/me/nizheg/en/activity/BrailleActivity.java
@@ -0,0 +1,45 @@
+package me.nizheg.en.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import me.nizheg.en.R;
+import me.nizheg.en.service.SimpleBrailleService;
+
+public class BrailleActivity extends Activity {
+
+    private SimpleBrailleService simpleBrailleService = new SimpleBrailleService();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.braille);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        calculateAndDisplayBraille();
+    }
+
+    public void onToggleClicked(View view) {
+        calculateAndDisplayBraille();
+    }
+
+    private void calculateAndDisplayBraille() {
+        boolean isChecked1 = ((ToggleButton) findViewById(R.id.toggleButton1)).isChecked();
+        boolean isChecked2 = ((ToggleButton) findViewById(R.id.toggleButton2)).isChecked();
+        boolean isChecked3 = ((ToggleButton) findViewById(R.id.toggleButton3)).isChecked();
+        boolean isChecked4 = ((ToggleButton) findViewById(R.id.toggleButton4)).isChecked();
+        boolean isChecked5 = ((ToggleButton) findViewById(R.id.toggleButton5)).isChecked();
+        boolean isChecked6 = ((ToggleButton) findViewById(R.id.toggleButton6)).isChecked();
+
+        char[] characterList = simpleBrailleService.getAllPossibleVariants(
+            new boolean[]{isChecked1, isChecked2, isChecked3, isChecked4, isChecked5, isChecked6});
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(characterList, 0, 4);
+    }
+}
--- src/me/nizheg/en/activity/MainActivity.java
+++ src/me/nizheg/en/activity/MainActivity.java
@@ -5,27 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.ToggleButton;
-
 import me.nizheg.en.R;
-import me.nizheg.en.service.SimpleBrailleService;
 
 public class MainActivity extends Activity {
-
-    private SimpleBrailleService simpleBrailleService = new SimpleBrailleService();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        calculateAndDisplayBraille();
     }
 
     @Override
@@ -41,6 +28,9 @@ public class MainActivity extends Activity {
             case R.id.action_open_browser:
                 openBrowserActivity();
                 return true;
+            case R.id.action_open_braille:
+                openBrailleActivity();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -51,22 +41,8 @@ public class MainActivity extends Activity {
         startActivity(intent);
     }
 
-    public void onToggleClicked(View view) {
-        calculateAndDisplayBraille();
-    }
-
-    private void calculateAndDisplayBraille() {
-        boolean isChecked1 = ((ToggleButton) findViewById(R.id.toggleButton1)).isChecked();
-        boolean isChecked2 = ((ToggleButton) findViewById(R.id.toggleButton2)).isChecked();
-        boolean isChecked3 = ((ToggleButton) findViewById(R.id.toggleButton3)).isChecked();
-        boolean isChecked4 = ((ToggleButton) findViewById(R.id.toggleButton4)).isChecked();
-        boolean isChecked5 = ((ToggleButton) findViewById(R.id.toggleButton5)).isChecked();
-        boolean isChecked6 = ((ToggleButton) findViewById(R.id.toggleButton6)).isChecked();
-
-        char[] characterList = simpleBrailleService.getAllPossibleVariants(
-                new boolean[]{isChecked1, isChecked2, isChecked3, isChecked4, isChecked5, isChecked6});
-
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(characterList, 0, 4);
+    private void openBrailleActivity() {
+        Intent intent = new Intent(this, BrailleActivity.class);
+        startActivity(intent);
     }
 }
