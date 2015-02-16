b98cd3aca47362cd528a1d0b5f39666dba0067f7
nizheg <nzhegalin@gmail.com>
2015-02-17 03:01:18 +0400

    Cosmetic changes

--- src/me/nizheg/en/activity/BrowserActivity.java
+++ src/me/nizheg/en/activity/BrowserActivity.java
@@ -1,7 +1,9 @@
 package me.nizheg.en.activity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.HttpAuthHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,25 +12,81 @@ import android.widget.EditText;
 import me.nizheg.en.R;
 
 public class BrowserActivity extends SubActivity {
+
+    WebView webView;
+    EditText urlView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browser);
         initializeWebView();
+        initializeUrlView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        openUrl();
     }
 
     private void initializeWebView() {
-        WebView myWebView = (WebView) findViewById(R.id.webView);
-        WebSettings webSettings = myWebView.getSettings();
+        this.webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setBuiltInZoomControls(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
         webSettings.setSupportZoom(true);
-        myWebView.setWebViewClient(new WebViewClient());
+        webSettings.setBuiltInZoomControls(true);
+        webView.setWebViewClient(new MyWebViewClient());
+    }
+
+    private void initializeUrlView() {
+        this.urlView = (EditText) findViewById(R.id.editText);
+        urlView.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    openUrl();
+                    return true;
+                }
+                return false;
+            }
+        });
+        urlView.setText("http://en-nizheg.rhcloud.com/");
+        openUrl();
     }
 
     public void onGoClicked(View view) {
-        WebView myWebView = (WebView) findViewById(R.id.webView);
-        EditText urlView = (EditText) findViewById(R.id.editText);
-        myWebView.loadUrl(urlView.getText().toString());
+        openUrl();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void openUrl() {
+        String url = urlView.getText().toString().trim();
+        if (url.length() == 0) {
+            return;
+        }
+        if (!url.startsWith("http://")) {
+            url = "http://" + url;
+            urlView.setText(url);
+        }
+        webView.loadUrl(url);
+    }
+
+    private static class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onReceivedHttpAuthRequest(WebView view,
+                                              HttpAuthHandler handler, String host, String realm) {
+            handler.proceed("nizheg", "12345");
+        }
     }
 }
